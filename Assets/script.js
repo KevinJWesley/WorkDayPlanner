2013e9943307ea94e485372eba1d66d4c575ca1b
kevinjwesley <kevin.john.wesley@gmail.com>
2021-04-29 19:42:54 -0600

    improved JS with a for loop

--- Assets/script.js
+++ Assets/script.js
@@ -50,15 +50,20 @@ $(document).ready(function () {
     localStorage.setItem(time, value);
   });
 
-  
   // display items in local storage into timeblocks
-  $("#hour-09 .description").val(localStorage.getItem("hour-09)"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10)"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11)"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12)"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13)"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14)"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15)"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16)"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17)"));
+
+for (let i = 9; i < 18; i++) {
+  
+  $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
+}
+
+  // $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  // $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  // $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  // $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  // $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  // $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  // $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  // $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  // $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
